9f7596ddc49b2ee599ef74d6e8d0ff564e668995
Sunghwan Bang <sunghwan2789@gmail.com>
2020-06-20 00:49:08 +0900

    소프트키보드를 사용하지 않도록 설정

--- app/src/main/java/sunghwan2789/calculator/MainActivity.java
+++ app/src/main/java/sunghwan2789/calculator/MainActivity.java
@@ -1,14 +1,23 @@
 package sunghwan2789.calculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText expressionEditText;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expressionEditText = findViewById(R.id.expressionEditText);
+        expressionEditText.setShowSoftInputOnFocus(false);
+        expressionEditText.requestFocus();
     }
 }
