e59309684bb08e9993f81c09a6236934b3360764
Sunghwan Bang <sunghwan2789@gmail.com>
2020-06-26 20:04:13 +0900

    계산기 핵심 로직 구현

--- app/src/main/java/sunghwan2789/calculator/core/Calculator.java
+++ app/src/main/java/sunghwan2789/calculator/core/Calculator.java
@@ -19,14 +19,45 @@ public class Calculator {
 
     public BigDecimal execute() {
         while (!commandQueue.isEmpty()) {
+            // 피연산자 처리
             if (commandQueue.peek() instanceof OperandCommand) {
                 operandStack.push(commandQueue.remove().execute(operandStack, operatorStack));
                 continue;
             }
 
+            // 괄호 처리
+            if (commandQueue.peek() instanceof ParenthesisCommand) {
+                switch (commandQueue.remove().getType()) {
+                    case OPEN_PARENTHESIS:
+                        operatorStack.push(commandQueue.remove());
+                        break;
+                    case CLOSE_PARENTHESIS:
+                        while (operatorStack.peek().getType() != Command.OPEN_PARENTHESIS) {
+                            operandStack.push(operatorStack.pop().execute(operandStack, operatorStack));
+                        }
+                        operatorStack.pop();
+                        break;
+                    default:
+                        throw new UnsupportedOperationException();
+                }
+            }
+
+            // 우선순위가 높은 연산자 먼저 처리
+            while (!operatorStack.isEmpty() && operatorStack.peek().compareTo(commandQueue.peek()) > 0) {
+                operandStack.push(operatorStack.pop().execute(operandStack, operatorStack));
+            }
+
             operatorStack.push(commandQueue.remove());
         }
 
-        return BigDecimal.ZERO;
+        while (!operatorStack.isEmpty()) {
+            operandStack.push(operatorStack.pop().execute(operandStack, operatorStack));
+        }
+
+        if (operandStack.size() != 1) {
+            throw new UnsupportedOperationException();
+        }
+
+        return operandStack.pop();
     }
 }
